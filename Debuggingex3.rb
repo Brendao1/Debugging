6e2dbf337715b6bc7de8c553dc139482f655723a
“Brenda” <odonnell.brenda@gmail.com>
2019-02-06 09:40:14 +0000

    committing ex1 and ex3

---
+++ Debuggingex3.rb
@@ -0,0 +1,17 @@
+def factorial(n)
+  product = n  # start with product= n
+  p "at the start product is #{n}"
+  while n > 1
+    n -= 1
+    p "we multiply #{product} by #{n}"
+    product *= n
+    p "we get #{product}"
+  end
+  product
+end
+
+factorial(5)
+# Intended output:
+#
+# > factorial(5)
+# => 120
---
+++ Week_1
@@ -0,0 +1,11 @@
+=begin
+def say_hello(name)
+  "hello #(name)"  # replace () with {}
+end
+=end
+
+def say_hello(name)
+  puts "hello #{name}"
+end
+
+say_hello("Brenda")
